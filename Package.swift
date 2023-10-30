433fdf0be787c72a38a085b18a0ff734ce6dd2cd
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-10-30 18:46:40 +0900

    format

--- Package.swift
+++ Package.swift
@@ -6,26 +6,23 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "PropertyTracer",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PropertyTracer",
             targets: ["PropertyTracer"]
-        ),
-        .executable(
-            name: "PropertyTracerClient",
-            targets: ["PropertyTracerClient"]
-        ),
+        )
     ],
     dependencies: [
-        // Depend on the Swift 5.9 release of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "PropertyTracerMacros",
             dependencies: [
@@ -33,14 +30,10 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "PropertyTracer", dependencies: ["PropertyTracerMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "PropertyTracerClient", dependencies: ["PropertyTracer"]),
-
-        // A test target used to develop the macro implementation.
+        .target(
+            name: "PropertyTracer",
+            dependencies: ["PropertyTracerMacros"]
+        ),
         .testTarget(
             name: "PropertyTracerTests",
             dependencies: [
