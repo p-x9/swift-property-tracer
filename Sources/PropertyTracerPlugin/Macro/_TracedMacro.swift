18fc67c3424d8e32155f3afd6ed5b095cfa65dbe
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-11-02 20:21:40 +0900

    add diagnoses to _TracedMacro

--- Sources/PropertyTracerPlugin/Macro/_TracedMacro.swift
+++ Sources/PropertyTracerPlugin/Macro/_TracedMacro.swift
@@ -10,7 +10,38 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-struct _TracedMacro {}
+struct _TracedMacro {
+    struct Arguments {
+        let callbackExpr: ExprSyntax
+
+        init(
+            callbackExpr: ExprSyntax
+        ) {
+            self.callbackExpr = callbackExpr
+        }
+    }
+
+    static func arguments(
+        of node: AttributeSyntax,
+        context: some MacroExpansionContext
+    ) -> Arguments? {
+        guard case let .argumentList(arguments) = node.arguments,
+              let firstElement = arguments.first?.expression else {
+            return nil
+        }
+
+        if let closureExpr = firstElement.as(ClosureExprSyntax.self) {
+            context.diagnose(
+                PropertyTracerMacroDiagnostic.closureIsNotSupported.diagnose(at: closureExpr)
+            )
+            return nil
+        }
+
+        return .init(
+            callbackExpr: firstElement
+        )
+    }
+}
 
 extension _TracedMacro: AccessorMacro {
     static func expansion(
@@ -19,23 +50,50 @@ extension _TracedMacro: AccessorMacro {
         in context: some MacroExpansionContext
     ) throws -> [AccessorDeclSyntax] {
         guard let variableDecl = declaration.as(VariableDeclSyntax.self) else {
+            context.diagnose(
+                PropertyTracerMacroDiagnostic.requiresVariableDeclaration.diagnose(at: declaration)
+            )
             return []
         }
 
-        guard variableDecl.bindingSpecifier.tokenKind == .keyword(.var) else {
+        guard variableDecl.isVar else {
+            context.diagnose(
+                PropertyTracerMacroDiagnostic.requiredMutableVariableDeclaration.diagnose(at: declaration)
+            )
             return []
         }
 
         guard let binding = variableDecl.bindings.first else {
+            context.diagnose(
+                PropertyTracerMacroDiagnostic.multipleVariableDeclarationIsNotSupported.diagnose(at: declaration)
+            )
+            return []
+        }
+
+        guard binding.isStored else {
+            context.diagnose(
+                PropertyTracerMacroDiagnostic.getterAndSetterShouldBeNil.diagnose(at: declaration)
+            )
             return []
         }
 
         guard let identifier = binding.pattern.as(IdentifierPatternSyntax.self) else {
             return []
         }
-        //        guard let type = binding.typeAnnotation?.type.trimmed else {
-        //            return []
-        //        }
+
+        guard let type = binding.typeAnnotation?.type.trimmed else {
+            context.diagnose(
+                PropertyTracerMacroDiagnostic.specifyTypeExplicitly.diagnose(at: declaration)
+            )
+            return []
+        }
+
+        let argument = arguments(of: node, context: context)
+
+
+        let setCallback = """
+            _\(identifier).setCallback(\(argument?.callbackExpr.description ?? "nil"))
+            """
 
         return [
             """
@@ -48,10 +106,12 @@ extension _TracedMacro: AccessorMacro {
             """
             get {
                 _\(identifier).setParent(self)
+                \(raw: setCallback)
                 return _\(identifier).wrappedValue
             }
             set {
                 _\(identifier).setParent(self)
+                \(raw: setCallback)
                 _\(identifier).wrappedValue = newValue
             }
             """
